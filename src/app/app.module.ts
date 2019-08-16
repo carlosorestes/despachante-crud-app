1f977fa3e2326072cf6ff8e55f53809cff442034
carlosmalaquias <carlosomalaquias@gmail.com>
2019-08-15 23:47:15 -0300

    Adicionando Menu - Item Pessoas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,44 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PessoaModule } from './pessoa/pessoa.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { 
+  MatButtonModule,
+  MatCardModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatProgressSpinnerModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule } from  '@angular/material';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    PessoaModule
+    PessoaModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatButtonModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatListModule,
+    MatProgressSpinnerModule, 
+    MatSelectModule,
+    MatSidenavModule,  
+    MatTableModule,
+    MatToolbarModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
